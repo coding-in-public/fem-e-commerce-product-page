a2d16e0c5a3671f0b9af9df9843d3693fc7b7dd2
Chris Pennington <chris@codinginpublic.dev>
2022-02-06 13:56:27 -0700

    pre third video

---
+++ js/nav.js
@@ -0,0 +1,10 @@
+const closeMenuBtn = document.querySelector('#closeMenu');
+const openMenuBtn = document.querySelector('#openMenu');
+const navPanelContainer = document.querySelector('.nav__panel-container');
+
+function toggleMenu(){
+  navPanelContainer.classList.toggle('expanded');
+}
+
+closeMenuBtn.addEventListener('click', toggleMenu);
+openMenuBtn.addEventListener('click', toggleMenu);
